68d04d3a33a368816b816ff5f41bd7e855b919c5
Daniel De La Pava <daniel.suarez@wolox.co>
2021-01-18 17:46:21 -0500

    added crud methods for book

---
+++ src/main/java/wolox/training/exceptions/BookIdMismatchException.java
@@ -0,0 +1,24 @@
+package wolox.training.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
+public class BookIdMismatchException extends RuntimeException {
+
+    public BookIdMismatchException() {
+        super("Ids mismatch");
+    }
+
+    public BookIdMismatchException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public BookIdMismatchException(final String message) {
+        super(message);
+    }
+
+    public BookIdMismatchException(final Throwable cause) {
+        super(cause);
+    }
+}
