6ee037cb3b930b75037fb63f607fa7b225432119
Daniel De La Pava <daniel.suarez@wolox.co>
2021-01-19 09:03:04 -0500

    Optional is added to the function that searches for a book by author

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,5 +1,6 @@
 package wolox.training.repositories;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import wolox.training.models.Book;
@@ -7,5 +8,5 @@ import wolox.training.models.Book;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    Book findByAuthor(String author);
+    Optional<Book> findByAuthor(String author);
 }
