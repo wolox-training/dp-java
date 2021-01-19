2b120aa95952fe24be34f618d3ed56c08bd32c88
Daniel De La Pava <daniel.suarez@wolox.co>
2021-01-19 15:47:56 -0500

    User repository added

---
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -0,0 +1,18 @@
+package wolox.training.repositories;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import wolox.training.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    /**
+     * This method consults a user by name
+     *
+     * @param name: is the username
+     * @return {@link User}
+     */
+    Optional<User> findByName(String name);
+}
