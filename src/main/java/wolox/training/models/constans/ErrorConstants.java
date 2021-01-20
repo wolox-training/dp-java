8bb173c6bdd1bc541994179dbc4765f08d6ccd07
Daniel De La Pava <daniel.suarez@wolox.co>
2021-01-20 18:33:23 -0500

    The fields were changed to definitive and a constructor was added at the suggestion of sonarlint

--- src/main/java/wolox/training/models/constans/ErrorConstants.java
+++ src/main/java/wolox/training/models/constans/ErrorConstants.java
@@ -5,13 +5,17 @@ package wolox.training.models.constans;
  */
 public class ErrorConstants {
 
+    private ErrorConstants() {
+        throw new IllegalStateException("Constants class");
+    }
+
     /**
      * Preconditions message
      */
-    public static String NOT_NULL = "Please check the %s supplied, its null!.";
-    public static String NOT_EMPTY = "Please check the %s supplied, its empty!.";
-    public static String NOT_NUMERIC = "The %s isn't numeric.";
-    public static String NOT_GREATER_THAN = "The %s cannot smaller that %s.";
-    public static String NOT_LATER_CURRENT_DATE = "The %s cannot be later than the current date.";
+    public static final String NOT_NULL = "Please check the %s supplied, its null!.";
+    public static final String NOT_EMPTY = "Please check the %s supplied, its empty!.";
+    public static final String NOT_NUMERIC = "The %s isn't numeric.";
+    public static final String NOT_GREATER_THAN = "The %s cannot smaller that %s.";
+    public static final String NOT_LATER_CURRENT_DATE = "The %s cannot be later than the current date.";
 
 }
