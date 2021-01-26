35fb790c84d00ac1396a69c6155e710b421cbecc
Daniel De La Pava <daniel.suarez@wolox.co>
2021-01-26 09:25:06 -0500

    Updated the swagger api documentation

--- src/main/java/wolox/training/controllers/SecurityController.java
+++ src/main/java/wolox/training/controllers/SecurityController.java
@@ -4,7 +4,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import java.security.Principal;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wolox.training.exceptions.UserNotFoundException;
-import wolox.training.models.Book;
 import wolox.training.models.User;
 import wolox.training.repositories.UserRepository;
 
@@ -28,16 +26,19 @@ public class SecurityController {
         this.userRepository = userRepository;
     }
 
+    /**
+     * This method return current user authenticated
+     *
+     * @return {@link User}
+     */
     @GetMapping("/username")
-    @ApiOperation(value = "Giving an author, return one book", response = Book.class)
+    @ApiOperation(value = "Authenticated user's username", response = User.class)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Success"),
             @ApiResponse(code = 401, message = "Not Authorized"),
-            @ApiResponse(code = 403, message = "Access forbidden"),
-            @ApiResponse(code = 404, message = "User Not Found"),
+            @ApiResponse(code = 403, message = "Access forbidden")
     })
     public User currentUserName() {
-
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication instanceof AnonymousAuthenticationToken) {
             throw new BadCredentialsException("User not logged");
@@ -45,6 +46,5 @@ public class SecurityController {
 
         return userRepository.findByUsername(authentication.getName()).orElseThrow(
                 UserNotFoundException::new);
-
     }
 }
