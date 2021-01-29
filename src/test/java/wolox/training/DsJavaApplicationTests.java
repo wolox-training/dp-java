c8ba40fb6ca30c4b09d070e6904995cab3277fa8
Daniel De La Pava <daniel.suarez@wolox.co>
2021-01-29 15:07:54 -0500

    Added environment variable setting for testing

--- src/test/java/wolox/training/DsJavaApplicationTests.java
+++ src/test/java/wolox/training/DsJavaApplicationTests.java
@@ -2,8 +2,12 @@ package wolox.training;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = {"classpath:application-test.properties"})
+@ActiveProfiles("test")
 class DsJavaApplicationTests {
 
 	@Test
